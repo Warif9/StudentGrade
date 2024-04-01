34ed46ad0a85ab9b1aae7596be0ff420385a1940
Warif9 <151781921+Warif9@users.noreply.github.com>
2024-04-01 05:49:30 +0300

    Create StudentGradeTest.java

---
+++ StudentGradeTest.java
@@ -0,0 +1,58 @@
+package studentgrade;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/* JUnit 3.8.1 */
+public class StudentGradeTest extends TestCase {
+    
+    public StudentGradeTest(String testName) {
+        super(testName);
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    public void testGetGrade95() {
+        System.out.println("getGrade 95");
+        int mark = 95;
+        String expResult = "A";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+
+    public void testGetGrade85() {
+        System.out.println("getGrade 85");
+        int mark = 85;
+        String expResult = "B";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+    
+    public void testGetGrade75() {
+        System.out.println("getGrade 85");
+        int mark = 75;
+        String expResult = "F";
+        String result = StudentGrade.getGrade(mark);
+        assertEquals(expResult, result);
+    }
+
+  
+}
